dd3bbf7f0c4da51ceffa24dee24ce23bcbcf965f
00009474 <student00009474@gmail.com>
2020-12-08 15:18:47 +0500

    added comments

--- scripts/slider.js
+++ scripts/slider.js
@@ -3,6 +3,6 @@ setInterval(function(){
 document.getElementById('radio' + counter).checked = true;
 counter++;
 if(counter > 4) {
-    counter = 1; //returns to the first image
+    counter = 1; //returns to the first image, when it is approaches the last image
 }
 }, 4500); // time to change picture to another
